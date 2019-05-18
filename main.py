e66a602e81f97f096d6552fd364555c0d1efc213
RachithP <rachithprakash@gmail.com>
2019-05-18 16:57:35 -0400

    Adding changes to detction part

--- main.py
+++ main.py
@@ -103,8 +103,10 @@ def main():
 
 	blue_lower = np.array([80, 90, 0], np.uint8)
 	blue_upper = np.array([200, 200, 255], np.uint8)
-	# red_lower = np.array([20, 20, 100], np.uint8)
-	# red_upper = np.array([40, 200, 255], np.uint8)
+	red_lower_one = np.array([0, 0, 0], np.uint8)
+	red_upper_one = np.array([20, 255, 255], np.uint8)
+	red_lower_two = np.array([160, 0, 0], np.uint8)
+	red_upper_two = np.array([180, 255, 255], np.uint8)
 
 	path_to_images = 'denoised_images'
 	files = glob.glob(path_to_images + "/*.jpg")
@@ -115,27 +117,34 @@ def main():
 
 	for index in range(len(files)):
 
-		image_denoise = cv2.imread(path_to_images+"/frame"+str(index)+".jpg", -1)  # read image
+		image_denoise = cv2.imread(path_to_images + "/frame" + str(index) + ".jpg", -1)  # read image
 
-		image_denoise_hsv = cv2.imread(path_to_images_hsv+"/frame"+str(index)+".jpg", -1)
+		image_denoise_hsv = cv2.imread(path_to_images_hsv + "/frame" + str(index) + ".jpg", -1)
 
 		blue_hsv = cv2.inRange(image_denoise_hsv, blue_lower, blue_upper)
 
-		# showImage(np.hstack((image_denoise, image_denoise_hsv)))
+		red_hsv_one = cv2.inRange(image_denoise_hsv, red_lower_one, red_upper_one)
+		red_hsv_two = cv2.inRange(image_denoise_hsv, red_lower_two, red_upper_two)
+
+		red_hsv = np.maximum(red_hsv_one, red_hsv_two)
+
+		# showImage(np.hstack((red_hsv_one, red_hsv_two, red_hsv)))
 		# continue
 
 		b_, g_, r_ = cv2.split(image_denoise)  # split into channels
 
-		# b = contractStretching(b_).astype(float)  # adaptive histogram equivalent on each channel
-		# g = contractStretching(g_).astype(float)
-		# r = contractStretching(r_).astype(float)
+		b = contractStretching(b_).astype(float)  # adaptive histogram equivalent on each channel
+		g = contractStretching(g_).astype(float)
+		r = contractStretching(r_).astype(float)
 
-		b = imadjust(b_, tol=60).astype(float)
-		g = imadjust(g_, tol=60).astype(float)
-		r = imadjust(r_, tol=60).astype(float)
+		# b = imadjust(b_, tol=60).astype(float)
+		# g = imadjust(g_, tol=60).astype(float)
+		# r = imadjust(r_, tol=60).astype(float)
 
 		# showImage(np.hstack((b.astype(np.uint8), g.astype(np.uint8), r.astype(np.uint8))))
 		# continue
+
+# --------------Applying the ref paper's criteria for thresholding-------------------------
 		den = r + g + b  # calculate the denominator
 
 		red_image = np.maximum(0.0, np.minimum(r - b, r - g) / den)
@@ -149,37 +158,41 @@ def main():
 		blue_image = blue_image.astype(np.uint8)
 
 		blue_image = contractStretching(blue_image)
-		blue_thresh = cv2.threshold(blue_image, 160, 255, cv2.THRESH_BINARY)[1]
 		red_image = contractStretching(red_image)
-		red_thresh = cv2.threshold(red_image, 80, 255, cv2.THRESH_BINARY)[1]
+
+# ----------------------threshold the contrast image-----------------------------------
+		blue_thresh = cv2.threshold(blue_image, 150, 255, cv2.THRESH_BINARY)[1]
+		red_thresh = cv2.threshold(red_image, 150, 255, cv2.THRESH_BINARY)[1]
 
 		# -------------------------------------------------------
 		blue_image = np.maximum(blue_hsv, blue_thresh)
-		# showImage(np.hstack((blue_image, red_thresh)))
+		red_image = np.maximum(red_hsv, red_thresh)
+
+		# showImage(np.hstack((blue_image, red_image)))
 		# continue
+
 		final_image = image_denoise.copy()
 
-		_, red_contours, _ = cv2.findContours(red_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		_, red_contours, _ = cv2.findContours(red_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 		# for cnt in red_contours:
 		# 	x, y, w, h = cv2.boundingRect(cnt)
 		# 	cv2.rectangle(image_denoise, (x, y), (x + w, y + h), (0, 0, 255), 2)
-		# 	if 150.0 < cv2.contourArea(cnt) < 5000.0:
+		# 	if 150.0 < cv2.contourArea(cnt) < 4000.0:
 		# 		x, y, w, h = cv2.boundingRect(cnt)
 		# 		cv2.rectangle(image_denoise, (x, y), (x + w, y + h), (0, 255, 255), 2)
 		# 		x, y, w, h = cv2.boundingRect(cnt)
-		# 		if 0.3 < float(h) / w < 3.3:
+		# 		if 0.4 < float(h) / w < 2.5:
 		# 			cv2.rectangle(final_image, (x, y), (x + w, y + h), (200, 0, 200), 2)
-					
 
 		_, blue_contours, _ = cv2.findContours(blue_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 		for cnt in blue_contours:
 			x, y, w, h = cv2.boundingRect(cnt)
 			cv2.rectangle(image_denoise, (x, y), (x + w, y + h), (255, 0, 0), 2)
-			if 150.0 < cv2.contourArea(cnt) < 5000.0:
+			if 250.0 < cv2.contourArea(cnt) < 5000.0:
 				x, y, w, h = cv2.boundingRect(cnt)
 				cv2.rectangle(image_denoise, (x, y), (x + w, y + h), (255, 255, 0), 2)
 				x, y, w, h = cv2.boundingRect(cnt)
-				if 0.3 < float(h) / w < 3.3:
+				if 0.3 < float(h) / w < 3.0:
 					cv2.rectangle(final_image, (x, y), (x + w, y + h), (255, 100, 100), 2)
 					croppedWindow = final_image[y:y+h,x:x+w]
 					prediction = classifier(croppedWindow,hog,two_class_model)
@@ -187,7 +200,7 @@ def main():
 
 		cv2.imwrite("outputs/testing/frame"+str(index)+".jpg", image_denoise)
 		cv2.imwrite("outputs/testing/final/frame"+str(index)+".jpg", final_image)
-		showImage(image_denoise)
+		showImage(np.hstack((image_denoise, final_image)))
 		continue
 
 		# # -------------------------------------------------------
